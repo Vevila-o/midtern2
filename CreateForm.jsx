7bdb29627c30ee2afb7bbb68c9b486643a996fd2
Vevila-o <ze0980023@gmail.com>
2025-11-28 20:45:50 +0800

    Update CreateForm.jsx

--- CreateForm.jsx
+++ CreateForm.jsx
@@ -1,7 +1,11 @@
 function CreateForm(){
+    const [content, setContent ]=useState('');
+    
     return (
     <form>
-        <input type="text" placeholder="輸入代辦事項"/> 
+        <input type="text" placeholder="輸入代辦事項"
+        value = {content } 
+        onChange={()=>{setContent(e.target.value) }}/>
         <button type="submit">加入</button>
     
     <form/>
